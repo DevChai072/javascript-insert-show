e669ee8be8f0b016cb4d5ff6f11e8dd07545d12b
Devchai072 <devchai072@gmail.com>
2020-03-20 19:00:19 +0700

    commit change

--- myValidate.js
+++ myValidate.js
@@ -4,35 +4,58 @@
  * @param {*} objItemInput 
  */
 function validateForm(objItemInput) {
-    var counter = 0; // is variable for check validation true all
+    //var counter = 0; // is variable for check validation true all
 
-    Object.keys(objItemInput).forEach(function(item) {
-        // refresh name form tag alert span
-        var spnAlertName = objItemInput[item].id.charAt(0).toUpperCase() + objItemInput[item].id.slice(1);
-        defaultTagSpan(spnAlertName);
-        // check is tag name {input}
-        if (objItemInput[item].tagName === "INPUT") {
-            // check is length of value
-            if (objItemInput[item].value.length < 20 || objItemInput[item].value.length > 50) {
-                document.getElementById("spnAlertTxtFullname").innerHTML = "ระบุตัวอักษรระหว่าง 20-50 ตัวอักษร";
-                document.getElementById("spnAlertTxtFullname").style.color = "red";
-            } else {
-                counter++; // is true
-            }
-        // check is tag name {input}
-        } else if (objItemInput[item].tagName === "TEXTAREA") {
-            // check is length of value
-            if (objItemInput[item].value.length < 50 || objItemInput[item].value.length > 100) {
-                document.getElementById("spnAlertTxtAddress").innerHTML = "ระบุตัวอักษรระหว่าง 50-100 ตัวอักษร";
-                document.getElementById("spnAlertTxtAddress").style.color = "red";
-            } else {
-                counter++; // is true
-            }
+    var isValid = true;
 
-        }
-    });
+    // objItemInput = {
+    //     'inputFullname': document.getElementById("txtFullname"),
+    //     'inputAddress': document.getElementById("txtAddress")
+    // }
 
-    if (counter >= Object.keys(objItemInput).length) {
-        return isValid = true;
+    document.getElementById("spnAlertTxtFullname").innerHTML = "";
+    document.getElementById("spnAlertTxtAddress").innerHTML = "";
+    if (objItemInput.inputFullname.value.length < 20 || objItemInput.inputFullname.value.length > 50 ){
+        document.getElementById("spnAlertTxtFullname").innerHTML = "ระบุตัวอักษรระหว่าง 20-50 ตัวอักษร";
+        document.getElementById("spnAlertTxtFullname").style.color = "red";
+        isValid = false;
     }
+    
+    if (objItemInput.inputAddress.value.length < 50 || objItemInput.inputAddress.value.length > 100){
+        document.getElementById("spnAlertTxtAddress").innerHTML = "ระบุตัวอักษรระหว่าง 50-100 ตัวอักษร";
+        document.getElementById("spnAlertTxtAddress").style.color = "red";
+        isValid = false;
+    }
+
+    return isValid;
+
+    //Object.keys(objItemInput).forEach(function(item) {
+        // refresh name form tag alert span
+    //     var spnAlertName = objItemInput[item].id.charAt(0).toUpperCase() + objItemInput[item].id.slice(1);
+    //     defaultTagSpan(spnAlertName);
+    //     // check is tag name {input}
+    //     if (objItemInput[item].tagName === "INPUT") {
+    //         // check is length of value
+    //         if (objItemInput[item].value.length < 20 || objItemInput[item].value.length > 50) {
+    //             document.getElementById("spnAlertTxtFullname").innerHTML = "ระบุตัวอักษรระหว่าง 20-50 ตัวอักษร";
+    //             document.getElementById("spnAlertTxtFullname").style.color = "red";
+    //         } else {
+    //             counter++; // is true
+    //         }
+    //     // check is tag name {input}
+    //     } else if (objItemInput[item].tagName === "TEXTAREA") {
+    //         // check is length of value
+    //         if (objItemInput[item].value.length < 50 || objItemInput[item].value.length > 100) {
+    //             document.getElementById("spnAlertTxtAddress").innerHTML = "ระบุตัวอักษรระหว่าง 50-100 ตัวอักษร";
+    //             document.getElementById("spnAlertTxtAddress").style.color = "red";
+    //         } else {
+    //             counter++; // is true
+    //         }
+
+    //     }
+    // });
+
+    // if (counter >= Object.keys(objItemInput).length) {
+    //     return isValid = true;
+    // }
 }
